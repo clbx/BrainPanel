ad7b9b1a94892e08b3cc572f31d4e58eb4fac51a
Clay Buxton <clay@clbx.io>
2018-04-07 23:48:04 -0400

    Base Dummy Panel

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 BrainPanel is a front-end panel that displays information received from the output of a Muse Brain sensor.
 
-BrainPanel runs off flask and jinja2. 
+BrainPanel runs off flask and jinja2.
 
 
 ##### Requirements:
 
 Python 3.6+
 Flask
+pygal
--- getdata.py
+++ getdata.py
@@ -1,14 +1,35 @@
 import csv
+import pygal
+import json
 from flask import Flask
 from flask import render_template
+from urllib.request import urlopen
 
 app = Flask(__name__)
+
+def chart():
+    graph = pygal.Line(fill=True)
+    graph.title = 'Brain Readings'
+    graph.y_labels = ['0%','20%','40%','60%','80%','100%']
+    graph.add("Alpha", [20, 23, 34, 34, 65, 56, 78, 45])
+    graph.add("Beta", [34, 45, 56, 44, 67, 45, 34, 69])
+    graph_data = graph.render_data_uri()
+    return graph_data
+
+def chart1():
+    graph = pygal.Line(fill=True)
+    graph.title = 'Brain Readings'
+    graph.y_labels = ['0%','20%','40%','60%','80%','100%']
+    graph.add("Alpha", [45, 56, 23, 32, 45, 23, 45, 55])
+    graph.add("Beta", [60, 34, 44, 25, 67, 30, 55, 60])
+
+    graph_data = graph.render_data_uri()
+    return graph_data
+
 
 def readMyFile(filename):
     times = []
     element = []
-
-
 
     with open(filename) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
@@ -32,9 +53,16 @@ while i < len(element):
 print(blinks)
 print(clench)
 
+@app.route('/line.svg')
+def line_route():
+    chart = pygal.Line()
+    return char.render_response
+
+blinked = 556
+said = 345
+time = 345
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home', chart = chart(), chart1 = chart1(), blinked = blinked, said = said, time = time)
